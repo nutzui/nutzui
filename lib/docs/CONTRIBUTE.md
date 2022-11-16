6db6c8d61c2cba41b5d605abe3ecd9a6a92dd5fa
Robert Heessels <robert@heessels.com>
2022-11-16 23:10:31 +0100

    Button can now have an icon.

--- lib/docs/CONTRIBUTE.md
+++ lib/docs/CONTRIBUTE.md
@@ -6,3 +6,10 @@ https://heroicons.dev
 or do a Google search: https://www.google.com/search?q=svg+icon+arrow+down
 
 Place the icons inside /lib/components/icons.
+
+## Flags
+
+Flag svg files:
+https://github.com/lipis/flag-icons/tree/main/flags/4x3
+
+Place the icons inside /lib/components/icons/flags.
--- lib/src/components/NuButton.vue
+++ lib/src/components/NuButton.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
 import './nutzui.styl'
+import { useSlots } from 'vue'
 
 defineProps<{
   title: string
 }>()
+
+const slots = useSlots()
 </script>
 
 <template>
-  <button>
-    {{ title }}
+  <button class="nu-flex nu-center-vert nu-center-horz">
+    <NuIcon v-if="slots.icon" class="nu-m-r-sm">
+      <slot name="icon"></slot>
+    </NuIcon>
+  {{ title }}
   </button>
 </template>
 
